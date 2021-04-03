87ec8497effa151cc658ac3aa1279a5a41872720
UniverseStark <jty90k@gmail.com>
2021-04-03 20:55:22 +0900

    1.1GET MOVIES Query

--- Documents/Nomad/apollo_moive-2021/src/routes/Home.js
+++ Documents/Nomad/apollo_moive-2021/src/routes/Home.js
@@ -1,3 +1,25 @@
 import React from "react";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/client";
+// JavaScrip에 GraphQL을 작성한다. 그러나 JavaScript는 GraphQL을 이해하지 못한다.
+// query 작성, query는 component 바깥에 위치한다.
 
-export default () => <h1>Home</h1>;
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
+
+//component
+export default () => {
+  const { loading, error, data } = useQuery(GET_MOVIES);
+  if (loading) {
+    return "Loading...";
+  }
+  if (data && data.movies) {
+    return data.movies.map((m) => <h1>{m.id}</h1>);
+  }
+};
