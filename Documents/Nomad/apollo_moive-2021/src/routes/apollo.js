c5eb5c70de6ef4c23c507cb330a95dd9f571c1a9
UniverseStark <jty90k@gmail.com>
2021-04-05 16:26:44 +0900

    2.0 Local State part One

--- Documents/Nomad/apollo_moive-2021/src/routes/apollo.js
+++ Documents/Nomad/apollo_moive-2021/src/routes/apollo.js
@@ -1,8 +1,12 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import ApolloClient from "apollo-boost";
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/",
-  cache: new InMemoryCache(),
+  resolvers: {
+    Movie: {
+      isLiked: () => false,
+    },
+  },
 });
 
 export default client;
