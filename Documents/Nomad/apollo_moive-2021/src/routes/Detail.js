32b1ff7c320b69b1f9f673734a0905efc265610b
UniverseStark <jty90k@gmail.com>
2021-04-05 20:12:57 +0900

    2.1 Local State part Two

--- Documents/Nomad/apollo_moive-2021/src/routes/Detail.js
+++ Documents/Nomad/apollo_moive-2021/src/routes/Detail.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import { gql } from "apollo-boost";
-import { useQuery } from "@apollo/client";
+import { gql, useQuery } from "@apollo/client";
 import styled from "styled-components";
 
 const GET_MOVIE = gql`
--- Documents/Nomad/apollo_moive-2021/src/routes/Home.js
+++ Documents/Nomad/apollo_moive-2021/src/routes/Home.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { gql } from "apollo-boost";
-import { useQuery } from "@apollo/client";
+import { gql, useQuery } from "@apollo/client";
 import styled from "styled-components";
 import Movie from "../components/Movie";
 
