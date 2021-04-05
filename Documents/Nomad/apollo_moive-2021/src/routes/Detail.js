9b366a69e07c679e7105565cce9bb59b4929acd0
UniverseStark <jty90k@gmail.com>
2021-04-05 15:30:22 +0900

    1.5 Suggestions & Optional Chaining

--- Documents/Nomad/apollo_moive-2021/src/routes/Detail.js
+++ Documents/Nomad/apollo_moive-2021/src/routes/Detail.js
@@ -15,6 +15,10 @@ const GET_MOVIE = gql`
       rating
       description_intro
     }
+    suggestions(id: $id) {
+      id
+      medium_cover_image
+    }
   }
 `;
 const Container = styled.div`
@@ -60,22 +64,17 @@ export default () => {
   const { loading, data } = useQuery(GET_MOVIE, {
     variables: { id: +params.id },
   });
+  console.log(data);
   return (
     <Container>
       <Column>
         <Title>{loading ? "Loading..." : data.movie.title}</Title>
-        {!loading && data.movie && (
-          <>
-            <Subtitle>
-              {data.movie.language} - {data.movie.rating}
-            </Subtitle>
-            <Description>{data.movie.description_intro}</Description>
-          </>
-        )}
+        <Subtitle>
+          {data?.movie?.language} - {data?.movie?.rating}
+        </Subtitle>
+        <Description>{data?.movie?.description_intro}</Description>
       </Column>
-      <Poster
-        bg={data && data.movie ? data.movie.medium_cover_image : " "}
-      ></Poster>
+      <Poster bg={data?.movie?.medium_cover_image}></Poster>
     </Container>
   );
 };
--- Documents/Nomad/apollo_moive-2021/src/routes/Home.js
+++ Documents/Nomad/apollo_moive-2021/src/routes/Home.js
@@ -60,17 +60,15 @@ export default () => {
   return (
     <Container>
       <Header>
-        <Title>Apollo 2021</Title>
-        <Subtitle>I Love GraphQL</Subtitle>
+        <Title>Apollo 2020</Title>
+        <Subtitle>I love GraphQL</Subtitle>
       </Header>
-      {loading && <Loading>Loading ...</Loading>}
-      {!loading && data.movies && (
-        <Movies>
-          {data.movies.map((m) => (
-            <Movie key={m.id} id={m.id} bg={m.medium_cover_image} />
-          ))}
-        </Movies>
-      )}
+      {loading && <Loading>Loading...</Loading>}
+      <Movies>
+        {data?.movies?.map((m) => (
+          <Movie key={m.id} id={m.id} bg={m.medium_cover_image} />
+        ))}
+      </Movies>
     </Container>
   );
 };
